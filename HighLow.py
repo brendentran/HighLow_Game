03521b4523853147671ad40b83924aa12784d9ef
Brenden Tran <brenden.tran@bimodalconsulting.com>
2021-08-24 19:52:58 -0700

    Update HighLow.py

--- HighLow.py
+++ HighLow.py
@@ -10,24 +10,24 @@ input("Press Enter to start")
 guesses = 1
 while True:
         print("\tGuessing in the range of {} to {}".format(low, high))
-            guess = low + (high - low) // 2
-                high_low = input("My guess is {}. Should I guess higher or lower? "
-                                             "Enter h or l, or c if my guess was correct"
-                                                                  .format(guess)).casefold()
+        guess = low + (high - low) // 2
+        high_low = input("My guess is {}. Should I guess higher or lower? "
+                         "Enter h or l, or c if my guess was correct"
+                          .format(guess)).casefold()
 
-                    if high_low == "h":
-                                # Guess higher. The low end of the range becomes 1 greater than the guess.
-                                        low = guess + 1
-                                            elif high_low == "l":
-                                                        # Guess lower. The high end of the range becomes one less than the guess.
-                                                                high = guess - 1
-                                                                    elif high_low == "c":
-                                                                                print("I got it in {} guesses!".format(guesses))
-                                                                                        break
-                                                                                        else:
-                                                                                                    print("Please enter h, l or c")
+        if high_low == "h":
+                # Guess higher. The low end of the range becomes 1 greater than the guess.
+                low = guess + 1
+        elif high_low == "l":
+                # Guess lower. The high end of the range becomes one less than the guess.
+                high = guess - 1
+        elif high_low == "c":
+                print("I got it in {} guesses!".format(guesses))
+                break
+        else:
+                print("Please enter h, l or c")
+        
+        guesses = guesses + 1
 
-                                                                                                        guesses = guesses + 1
-
-                                                                                                        # A simple typing error could result in the program never guessing correctly, and we may not detect that unless we choose a number that causes a problem
-                                                                                                        # An augment assignment is a shorthand way of assigning values to a variable
+# A simple typing error could result in the program never guessing correctly, and we may not detect that unless we choose a number that causes a problem
+# An augment assignment is a shorthand way of assigning values to a variable
